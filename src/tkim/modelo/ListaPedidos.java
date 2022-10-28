3b9b28805204044b84ceb8a569478703b5818f65
kater <kater@LAPTOP-PF6T2F4D>
2022-10-28 17:13:33 +0200

    ListaPedido KatKateryna

--- src/tkim/modelo/ListaPedidos.java
+++ src/tkim/modelo/ListaPedidos.java
@@ -63,4 +63,23 @@ public class ListaPedidos {
 	        return resultado;
 
 	}
+	
+	
+    public ArrayList<Pedido> mostrarPedEnviados(String nif, Listas<Pedido> lped){
+        ArrayList<Pedido> pedidosEnviados = new ArrayList<>();
+        for (Pedido pd : lped.getDato()) {
+            if (nif.equals(pd.getCliente().getNif())){
+
+                Duration duration = Duration.between(pd.getFechaHoraPedido(), LocalDateTime.now());
+                long diff =  Math.abs(duration.toMinutes());
+
+                if (pd.getArticulos().getTiempoPreparacion() < diff){
+                    pedidosEnviados.add(pd);
+                }
+
+            }
+
+        }
+        return pedidosEnviados;
+    }
 }
