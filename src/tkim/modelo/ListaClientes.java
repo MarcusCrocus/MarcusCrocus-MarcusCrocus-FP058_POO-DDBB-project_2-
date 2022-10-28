05ab2c2e5e75fbd65dd663d2e54f55f16d078f23
marcus <azelenskyy@uoc.edu>
2022-10-28 16:25:16 +0200

    marc

--- src/tkim/modelo/ListaClientes.java
+++ src/tkim/modelo/ListaClientes.java
@@ -1,14 +1,13 @@
 package tkim.modelo;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import tkim.controlador.Controlador;
 
 public class ListaClientes extends Listas<Cliente> {
 	
 
 	
-	public String addClientes(String nif, String nombre, String domicilio, String email, String tipoCliente, Listas<Cliente> listasClientes) {
+	public String anadirCliente(String nombre, String domi, String nif, String mail, String tipoCliente, Listas<Cliente> listasClientes) {
 		
 
 		
@@ -16,14 +15,14 @@ public class ListaClientes extends Listas<Cliente> {
 					 
 				case "1":
 						 
-					Cliente ClienteEstandar = new ClienteEstandar(nif, nombre, domicilio, email,tipoCliente);
+					Cliente ClienteEstandar = new ClienteEstandar(nombre, domi, nif, mail);
 					listasClientes.addDato(ClienteEstandar);
 					
 					return "The Client was added: " + ClienteEstandar.getNif() +" "+ ClienteEstandar.getNombre();
 			
 				case "2": 
 					
-					Cliente ClientePeremium = new ClientePremium(nif, nombre, domicilio, email, tipoCliente);
+					Cliente ClientePeremium = new ClientePremium(nombre, domi, nif, mail);
 					listasClientes.addDato(ClientePeremium);
 			
 					return "The Client was added: " + ClientePeremium.getNif() +" "+ ClientePeremium.getNombre(); 
@@ -32,11 +31,56 @@ public class ListaClientes extends Listas<Cliente> {
 			return tipoCliente;
 	}
 
+	// Metodo para comprobar si cliente existe
+	
+	public boolean existeCliente(String nif,Listas<Cliente> cli) {
+			
+			
+			boolean existe = false;
+			
+				for (Cliente a : cli.getDato()) {
+					if (a.getNif().equals(nif)) {
+						existe = true;
+						break;
+					}
+				}
+			
+			return existe;
 
+		}
+		
+
+		
+	//Metodos para Mostrar Clientes Estandar y Cliente Premium	
+		
+		public List<Cliente> mostrarClienteEstandar(Listas<Cliente> clientes) {
+			
+			List<Cliente> clientesEstandar = new ArrayList<>();
+				for (Cliente var : clientes.getDato()) {
+					if (var.tipoCliente() == "Cliente Estandar") {
+						clientesEstandar.add(var);
+					}
+					
+				}
+
+			
+			return clientesEstandar;
+		}
+		
+		public List<Cliente> mostrarClientePremium(Listas<Cliente> clientes) {
+			
+			List<Cliente> showClientesPremium = new ArrayList<>();
+				for (Cliente var : clientes.getDato()) {
+					if (var.tipoCliente() == "Cliente Premium") {
+						showClientesPremium.add(var);
+					}
+					
+				}
+				
+
+			return showClientesPremium;
+		}
 
 
 }
-
-
-
 
