e39bc9e894d78f6b88f2bdfa34f197b40546c210
israe <israe@DESKTOP-N2CP7LV>
2022-10-28 11:08:33 +0200

    cliente estandar isra

--- src/tkim/modelo/ClienteEstandar.java
+++ src/tkim/modelo/ClienteEstandar.java
@@ -1,30 +1,35 @@
 package tkim.modelo;
 
 public class ClienteEstandar extends Cliente{
-
-	public ClienteEstandar(String nif, String nombre, String domicilio, String email, String tipoCliente) {
-		super(nif, nombre, domicilio, email);
-		
+	
+	public ClienteEstandar(String nombre, String domicilio, String nif, String email) {
+		super(nombre, domicilio, nif, email);	
 	}
 
 	@Override
 	public String tipoCliente() {
-		return "Cliente_Estandar";
+		// TODO Auto-generated method stub
+		return "Cliente Estandar";
 	}
+
+
 
 	@Override
 	public float calcAnual() {
+		// TODO Auto-generated method stub
 		return 0;
 	}
+
+
 
 	@Override
 	public float descuentoEnv() {
+		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	
 	
-
-
+	
 
 }
