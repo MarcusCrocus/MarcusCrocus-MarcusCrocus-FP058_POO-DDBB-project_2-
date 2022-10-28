4d612b11ac76864f0771e7817449b7f68e22ed95
israe <israe@host.docker.internal>
2022-10-28 17:22:35 +0200

    lista cliente isra

--- src/tkim/modelo/ListaClientes.java
+++ src/tkim/modelo/ListaClientes.java
@@ -1,42 +1,64 @@
 package tkim.modelo;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import tkim.controlador.Controlador;
-
 public class ListaClientes extends Listas<Cliente> {
-	
 
-	
-	public String addClientes(String nif, String nombre, String domicilio, String email, String tipoCliente, Listas<Cliente> listasClientes) {
+	public String anadirCliente(String nombre, String domi, String nif, String mail, String tipoCliente,
+			Listas<Cliente> cliente) {
 		
-
-		
-			switch (tipoCliente) { 
-					 
-				case "1":
-						 
-					Cliente ClienteEstandar = new ClienteEstandar(nif, nombre, domicilio, email,tipoCliente);
-					listasClientes.addDato(ClienteEstandar);
-					
-					return "The Client was added: " + ClienteEstandar.getNif() +" "+ ClienteEstandar.getNombre();
-			
-				case "2": 
-					
-					Cliente ClientePeremium = new ClientePremium(nif, nombre, domicilio, email, tipoCliente);
-					listasClientes.addDato(ClientePeremium);
-			
-					return "The Client was added: " + ClientePeremium.getNif() +" "+ ClientePeremium.getNombre(); 
-					
-			}
-			return tipoCliente;
+		try {
+			Cliente clientes = new ClienteEstandar(nombre, domi, nif, mail);
+			cliente.addDato(clientes);
+			return "El cliente ha sido añadido correctamente: " + clientes.getNombre();
+		} catch (Exception e) {
+			return "El cliente no ha podido introducirse correctamente";
+		}
 	}
 
+	public boolean ifExist(String nif, Listas<Cliente> c) {
+		boolean existe = false;
+		for (Cliente cliente : c.getDato()) {
+			if (cliente.getNif().equals(nif)) {
+				existe = true;
 
+				break;
+			}
 
+		}
+		return existe;
+	}
+
+	public List<Cliente> mostrarClienteEstandar(Listas<Cliente> c) {
+		List<Cliente> clientesStandard = new ArrayList<>();
+		for (Cliente cliente : c.getDato()) {
+			if (cliente.tipoCliente() == "Cliente Estandar") {
+				clientesStandard.add(cliente);
+			}
+		}
+		return clientesStandard;
+	}
+	
+	public List<Cliente> mostrarClientePremium(Listas<Cliente> c) {
+		List<Cliente> clientesPremium = new ArrayList<>();
+		for (Cliente cliente : c.getDato()) {
+			if (cliente.tipoCliente() == "Cliente Premium") {
+				clientesPremium.add(cliente);
+			}
+		}
+		return clientesPremium;
+	}
+	
+	public Boolean existeCliente(String nif, Listas<Cliente> c) {
+		Boolean existeCliente = false;
+		for (Cliente cliente : c.getDato()) {
+			if (cliente.getNif().equals(nif)) {
+				existeCliente = true;
+			}
+		}
+		
+		return existeCliente;
+	}
 
 }
-
-
-
-
