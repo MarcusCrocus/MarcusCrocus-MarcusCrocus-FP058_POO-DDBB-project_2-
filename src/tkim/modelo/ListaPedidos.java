1e51355dbbf0a7b99a9a5ad23736f7047c9295c9
israe <israe@host.docker.internal>
2022-10-29 08:07:44 +0200

    lista pedidos isra

--- src/tkim/modelo/ListaPedidos.java
+++ src/tkim/modelo/ListaPedidos.java
@@ -3,37 +3,37 @@ package tkim.modelo;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-
+import java.util.List;
 
 public class ListaPedidos {
 
-	public String addPedido(int numeroPedido, int unidadesPedido, LocalDateTime fechaHoraPedido, Cliente cliente, Articulo articulo, Listas<Pedido> p) {
+	public String addPedido(int numeroPedido, int unidadesPedido, LocalDateTime fechaHoraPedido, Cliente cliente,
+			Articulo articulo, Listas<Pedido> p) {
 		String mensaje = "";
 		try {
-			
+
 			Pedido pedido = new Pedido(numeroPedido, unidadesPedido, fechaHoraPedido, cliente, articulo);
 			p.addDato(pedido);
 			mensaje = "El pedido se ha insertado correctamente";
 		} catch (NullPointerException e) {
 			mensaje = "la clase no existe. Vuelve a intentarlo";
-		}
-		catch (ArithmeticException e) {
+		} catch (ArithmeticException e) {
 			mensaje = "la clase no existe. Vuelve a intentarlo";
 		}
-		
+
 		return mensaje;
 	}
-	
-	public ArrayList<Pedido> pedidosCliente(String nifCliente, Listas<Pedido> p){
+
+	public ArrayList<Pedido> pedidosCliente(String nifCliente, Listas<Pedido> p) {
 		ArrayList<Pedido> pedidoXcliente = new ArrayList<>();
 		for (Pedido pedido : p.getDato()) {
 			if (pedido.getCliente().getNif().equals(nifCliente)) {
-					
-			}	
+
+			}
 		}
-		return pedidoXcliente;		
+		return pedidoXcliente;
 	}
-	
+
 	public Boolean existePedido(int codigoPedido, Listas<Pedido> p) {
 		Boolean existeArticulo = false;
 		for (Pedido pedido : p.getDato()) {
@@ -41,47 +41,64 @@ public class ListaPedidos {
 				existeArticulo = true;
 			}
 		}
-		
+
 		return existeArticulo;
 	}
-	
+
 	public String eliminarPedido(int codigoPedido, Listas<Pedido> p) {
-	      String resultado = "El pedido no ha podido ser eliminado porque ya se ha enviado";
-	        for (Pedido pedido : p.getDato()) {
-	            if (codigoPedido == pedido.getNumeroPedido()){
+		String resultado = "El pedido no ha podido ser eliminado porque ya se ha enviado";
+		for (Pedido pedido : p.getDato()) {
+			if (codigoPedido == pedido.getNumeroPedido()) {
 
-	                Duration duration = Duration.between(pedido.getFechaHoraPedido(), LocalDateTime.now());
-	                long diff =  Math.abs(duration.toMinutes());
-	                boolean enviado_pendiente = pedido.getArticulos().getTiempoPreparacion() > diff;
+				Duration duration = Duration.between(pedido.getFechaHoraPedido(), LocalDateTime.now());
+				long diff = Math.abs(duration.toMinutes());
+				boolean enviado_pendiente = pedido.getArticulos().getTiempoPreparacion() > diff;
 
-	                if (enviado_pendiente){
-	                    p.getDato().remove(pedido);
-	                    resultado = "El pedido ha sido eliminado";
-	                }
-	            }
-	        }
-	        return resultado;
+				if (enviado_pendiente) {
+					p.getDato().remove(pedido);
+					resultado = "El pedido ha sido eliminado";
+				}
+			}
+		}
+		return resultado;
 
 	}
-	
-	
-    public ArrayList<Pedido> mostrarPedEnviados(String nif, Listas<Pedido> lped){
-        ArrayList<Pedido> pedidosEnviados = new ArrayList<>();
-        for (Pedido pd : lped.getDato()) {
-            if (nif.equals(pd.getCliente().getNif())){
 
-                Duration duration = Duration.between(pd.getFechaHoraPedido(), LocalDateTime.now());
-                long diff =  Math.abs(duration.toMinutes());
+	public List<Pedido> mostrarPedEnviados(String nif, Listas<Pedido> lped) {
 
-                if (pd.getArticulos().getTiempoPreparacion() < diff){
-                    pedidosEnviados.add(pd);
-                }
+		ArrayList<Pedido> pedidosEnviados = new ArrayList<>();
+		for (Pedido pd : lped.getDato()) {
+			if (nif.equals(pd.getCliente().getNif())) {
 
-            }
+				Duration duration = Duration.between(pd.getFechaHoraPedido(), LocalDateTime.now());
+				long diff = Math.abs(duration.toMinutes());
 
-        }
-        return pedidosEnviados;
-    }
-    
-    // hello
+				if (pd.getArticulos().getTiempoPreparacion() < diff) {
+					pedidosEnviados.add(pd);
+				}
+
+			}
+
+		}
+		return pedidosEnviados;
+	}
+
+	public List<Pedido> mostrarPedPendientes(String nif, Listas<Pedido> lped) {
+
+		ArrayList<Pedido> pedidosPendientes = new ArrayList<>();
+		for (Pedido pd : lped.getDato()) {
+			if (nif.equals(pd.getCliente().getNif())) {
+
+				Duration duration = Duration.between(pd.getFechaHoraPedido(), LocalDateTime.now());
+				long diff = Math.abs(duration.toMinutes());
+
+				if (pd.getArticulos().getTiempoPreparacion() > diff) {
+					pedidosPendientes.add(pd);
+				}
+
+			}
+
+		}
+		return pedidosPendientes;
+	}
 }
