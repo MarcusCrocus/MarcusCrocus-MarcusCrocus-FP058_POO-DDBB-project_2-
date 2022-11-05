6bf31d8739ab7bcf6bb54435903880a7f81b4cae
IsraCrocus <ipuyol@uoc.edu>
2022-11-05 17:17:51 +0100

    clase de lanzamiento del dato de Articulo

---
+++ src/tkim/modelo/LanzarArticuloDAO.java
@@ -0,0 +1,36 @@
+package tkim.modelo;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import tkim.dao.IArticuloDAO;
+
+public class LanzarArticuloDAO {
+
+	public String addArticulo(String codigo, String descripcion, float precioVenta, float gastosEnvio,
+			int tiempoPreparacion) {
+
+		try {
+			Articulo articulo = new Articulo(codigo, descripcion, precioVenta, gastosEnvio, tiempoPreparacion);
+			IArticuloDAO dao = (IArticuloDAO) FactoryDAOs.getDAO("Articulo");
+			return dao.addArticulos(articulo);
+
+		} catch (Exception e) {
+			return "El articulo no ha podido introducirse correctamente";
+		}
+	}
+	
+	public List<Articulo> mostrarArticulos(){
+		IArticuloDAO dao = (IArticuloDAO) FactoryDAOs.getDAO("Articulo");
+		return dao.mostrarArticulos();
+		
+	}
+	
+	public Boolean existeArticulo(String codigo) {
+		IArticuloDAO dao = (IArticuloDAO) FactoryDAOs.getDAO("Articulo");
+		return dao.existeArticulo(codigo);
+	}
+
+}
