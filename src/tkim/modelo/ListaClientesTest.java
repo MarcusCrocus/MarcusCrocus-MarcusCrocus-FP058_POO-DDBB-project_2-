41ece4ccf6e2eba73ac4ea4c164053412ec8d424
Marcus <crocushtml@gmail.com>
2022-11-06 15:35:59 +0100

    add classes interfase

--- src/tkim/modelo/ListaClientesTest.java
+++ src/tkim/modelo/ListaClientesTest.java
@@ -2,7 +2,6 @@ package tkim.modelo;
 
 import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
