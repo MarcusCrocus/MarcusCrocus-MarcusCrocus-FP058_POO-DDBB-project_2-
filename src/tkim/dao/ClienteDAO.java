55778b949e672a47cb2e04196d8466b1618417b7
IsraCrocus <ipuyol@uoc.edu>
2022-11-18 21:36:00 +0100

    clienteDAO sin head

--- src/tkim/dao/ClienteDAO.java
+++ src/tkim/dao/ClienteDAO.java
@@ -203,23 +203,6 @@ public class ClienteDAO implements IClientesDAO {
 		
 	}
 
-<<<<<<< HEAD
-	@Override
-	public Cliente buscarCliente(String nif) {
-		//buscas cliente en BBDD por nif
-		//del resulset miras que tipo de cliente es y haces el if/else
-		/*String tc = tipo_cli.replace(" ","");
-		if tipocliente estandar
-		Cliente cliente = new ClienteEstandar(nif, nombre, domicilio, email);
-		else
-		Cliente cliente = new ClientePremium(nif, nombre, domicilio, email);*/
-		//miras que tipo de cliente es y haces un if para meterlo como estandar o como premium
-		return null;
-	}
-=======
-
->>>>>>> MarcusCrocus
-
 	@Override
 	public List<Cliente> mostrarClientesTodos() {
 		registerDriver();
